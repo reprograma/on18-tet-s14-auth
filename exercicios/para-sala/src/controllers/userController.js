2ad5c3b978ac22bde0a00e31d492e866c4b85b1d
mflilian <mayhharamorais@gmail.com>
2022-11-11 23:38:39 -0300

    first commit

---
+++ exercicios/para-sala/src/controllers/userController.js
@@ -0,0 +1,14 @@
+const UserSchema = require("../models/userSchema");
+
+const getAll = async (req, res) => {
+  UserSchema.find(function (err, users) {
+    if(err) {
+      res.status(500).send({ message: err.message })
+    }
+      res.status(200).send(users)
+  }) 
+};
+
+module.exports = {
+  getAll
+};
